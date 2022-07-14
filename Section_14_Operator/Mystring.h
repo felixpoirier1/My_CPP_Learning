26273de88b81acc1fcb89ba112d2c2153331a83d
felixpoirier1 <86891384+felixpoirier1@users.noreply.github.com>
2022-07-14 16:14:46 -0400

    commit

---
+++ Section_14_Operator/Mystring.h
@@ -0,0 +1,25 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+#include <cstring>
+#include <iostream>
+
+class Mystring
+{
+private:
+    char *str;
+public:
+    Mystring();
+    Mystring(const char *s);
+    Mystring(const Mystring &source);
+    ~Mystring();
+
+    //overload
+    Mystring &operator=(const Mystring &rhs);   //copy assignment
+
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+};
+
+
+#endif
