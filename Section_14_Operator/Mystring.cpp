8f3fdf2b1a543875df927a57db7a926f527d927f
felixpoirier1 <86891384+felixpoirier1@users.noreply.github.com>
2022-07-14 16:14:55 -0400

    commit

---
+++ Section_14_Operator/Mystring.cpp
@@ -0,0 +1,53 @@
+#include "Mystring.h"
+
+Mystring::Mystring()
+    :str{nullptr}{
+        str = new char[1];
+        *str = '\0';
+}
+
+Mystring::Mystring(const char *s)
+    :str{nullptr}{
+        if (s==nullptr){
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[std::strlen(s)+1];
+            std::strcpy(str, s);
+        }
+
+}
+
+Mystring::Mystring(const Mystring &source)
+    :str{nullptr}{
+        str = new char[std::strlen(source.str)+1];
+        std::strcpy(str, source.str);
+    }
+
+Mystring::~Mystring(){
+    delete [] str;
+}
+
+//copy assignment (operator "=")
+Mystring &Mystring::operator=(const Mystring &rhs){
+    std::cout << "Copy assignment"<< std::endl;
+    if(this == &rhs)
+        return *this;
+    
+    delete [] this->str;
+    str = new char[std::strlen(rhs.str)+1];
+    std::strcpy(str, rhs.str);
+    return this;
+}
+
+
+
+void Mystring::display() const{
+    std::cout << str << " : " << get_length() <<std::endl;
+}
+int Mystring::get_length() const{
+    return std::strlen(str);
+}
+const char *Mystring::get_str() const{
+    return str;
+}
