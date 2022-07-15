f4139a5c9aa15e5e2749f6f169ce212d4f5162a7
felixpoirier1 <86891384+felixpoirier1@users.noreply.github.com>
2022-07-15 15:47:45 -0400

    commit

--- Section_14_Operator/Section_exercise/Mystring.cpp
+++ Section_14_Operator/Section_exercise/Mystring.cpp
@@ -1,1 +1,132 @@
 #include "Mystring.h"
+
+//no args constr
+Mystring::Mystring()
+    :str{nullptr}
+{
+    str = new char[1];
+    *str = '\0';
+
+}
+
+//args constr
+Mystring::Mystring(const char *s)
+    :str{nullptr}
+{
+    if(s==nullptr){
+        str = new char[1];
+        *str = '\0';
+
+    } else {
+        str = new char[std::strlen(s)+1];
+        std::strcpy(str, s);
+    }
+}
+
+//copy constr
+Mystring::Mystring(const Mystring &source){
+    str = new char[std::strlen(source.str)+1];
+    std::strcpy(str, source.str);
+}
+
+//move constr
+Mystring::Mystring(Mystring &&source)
+    :str{source.str}
+{
+    source.str = nullptr;
+}
+
+//destr
+Mystring::~Mystring()
+{
+    delete [] str;
+}
+
+//copy assignemnt
+Mystring &Mystring::operator=(const Mystring &source){
+    if(this == &source){
+        return *this;
+    }
+    else{
+        delete this->str;
+        str = new char[std::strlen(source.str)+1];
+        std::strcpy(str, source.str);
+        return *this;
+    }
+}
+
+//move assignment
+Mystring &Mystring::operator=(Mystring &&source){
+    if(this == &source){
+        return *this;
+    }
+    else{
+        delete this->str;
+        str = source.str;
+        source.str = nullptr;
+        return *this;
+    }
+}
+
+Mystring Mystring::operator-()const{
+    char *buff = new char[std::strlen(str)+1];
+    std::strcpy(buff, str);
+    for(size_t i=0; i<std::strlen(buff); i++){
+        buff[i] = std::tolower(buff[i]);
+    }
+    Mystring temp{buff};
+    delete [] buff;
+
+    return temp;
+}
+
+bool Mystring::operator==(const Mystring &rhs) const{
+    return (std::strcmp(str, rhs.str)==0);
+}
+bool Mystring::operator!=(const Mystring &rhs) const{
+    return !(str == rhs.str);
+}
+
+bool Mystring::operator>(const Mystring &rhs) const{
+    return (str > rhs.str);
+}
+bool Mystring::operator<(const Mystring &rhs) const{
+    return (str < rhs.str);
+}
+Mystring Mystring::operator+(const Mystring &rhs) const{
+    char *buff = new char[std::strlen(str) + std::strlen(rhs.str) +1];
+    std::strcpy(buff, str);
+    std::strcat(buff, rhs.str);
+
+    Mystring temp{buff};
+    delete [] buff;
+    return temp;
+}
+
+Mystring &Mystring::operator+=(const Mystring &rhs){
+    *this = *this + rhs;
+    return *this;
+}
+Mystring Mystring::operator*(int num) const{
+    Mystring temp;
+    for(size_t i=0; i<num; i++){
+        temp = temp + *this;
+    }
+
+    return temp;
+}
+Mystring &Mystring::operator*=(int num){
+    *this = *this * num;
+    return *this;
+}
+Mystring &Mystring::operator++(){
+    for(size_t i=0; i<std::strlen(str); i++){
+        str[i] = std::toupper(str[i]);
+    }
+    return *this;
+}                           
+Mystring Mystring::operator++(int){
+    Mystring temp (*this);
+    operator++();
+    return temp;
+}
--- Section_14_Operator/Section_exercise/Mystring.h
+++ Section_14_Operator/Section_exercise/Mystring.h
@@ -1,9 +1,12 @@
 #ifndef _MYSTRING_H_
 #define _MYSTRING_H_
 #include <cstring>
+#include <iostream>
 
 class Mystring
 {
+    friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
+    friend std::istream &operator>>(std::istream &in, Mystring &rhs);
 private:
     char *str;
 public:
@@ -15,7 +18,7 @@ public:
 
     Mystring &operator=(const Mystring &source);
     Mystring &operator=(Mystring &&source);
-    Mystring operator-(const Mystring &rhs) const;
+    Mystring operator-()const;
     bool operator==(const Mystring &rhs) const;
     bool operator!=(const Mystring &rhs) const;
     bool operator>(const Mystring &rhs) const;
@@ -28,117 +31,7 @@ public:
     Mystring operator++(int); 
 
 };
-//no args constr
-Mystring::Mystring()
-    :str{nullptr}
-{
-    str = new char[1];
-    str = '\0';
 
-}
-
-//args constr
-Mystring::Mystring(const char &s)
-    :str{nullptr}
-{
-    if(s==nullptr){
-        str = new char[1];
-        str = '\0';
-
-    } else {
-        str = new char[std::strlen(s)+1];
-        std::strcpy(str, s);
-    }
-}
-
-//copy constr
-Mystring::Mystring(const Mystring &source){
-    str = new char[std::strlen(source)+1];
-    std::strcpy(str, source);
-}
-
-//move constr
-Mystring::Mystring(Mystring &&source)
-    :str{source.str}
-{
-    source.str = nullptr;
-}
-
-//destr
-Mystring::~Mystring()
-{
-    delete [] str;
-}
-
-//copy assignemnt
-Mystring &Mystring::operator=(const Mystring &source){
-    if(this == source){
-        return *this;
-    }
-    else{
-        delete this->str;
-        str = new char[std::strlen(source.str)+1];
-        std::strcpy(str, source.str);
-        return *this;
-    }
-}
-
-//move assignment
-Mystring &Mystring::operator=(Mystring &&source){
-    if(this == source){
-        return *this;
-    }
-    else{
-        delete this->str;
-        str = source.str;
-        source.rhs = nullptr;
-        return *this;
-    }
-}
-
-Mystring Mystring::operator-(const Mystring &rhs){
-    buff = new char[std::strlen(rhs.str)+1];
-    std::strcpy(buff, rhs.str);
-    for(size_t i=0, std::srtlen(buff); i++){
-        buff[i] = std::tolower(buff[i]);
-    }
-    Mystring temp{buff};
-    delete [] buff;
-
-    return temp;
-}
-
-bool Mystring::operator==(const Mystring &rhs) const{
-    return (std::strcmp(str, rhs.str)==0);
-}
-bool Mystring::operator!=(const Mystring &rhs) const{
-    return !(str == rhs.str);
-}
-
-bool Mystring::operator>(const Mystring &rhs) const{
-    return (str > rhs.str);
-}
-bool Mystring::operator<(const Mystring &rhs) const{
-    return (str < rhs.str);
-}
-Mystring Mystring::operator+(const Mystring &rhs) const{
-    buff = new char[std::strlen(str) + std::strlen(rhs.str) +1];
-    std::strcpy(buff, str);
-    std::strcat(buff, rhs.str);
-
-    Mystring temp{buff};
-    delete [] buff;
-    return temp;
-}
-
-Mystring &Mystring::operator+=(const Mystring &rhs){
-    *this = *this + rhs;
-    return *this;
-}
-Mystring Mystring::operator*(int num) const;
-Mystring &Mystring::operator*=(int num);
-Mystring &Mystring::operator++();                                     
-Mystring Mystring::operator++(int); 
 
 
 
